4f7c733c97f3a87d1e449d6231795761b10de205
Daniel Freire <danjfreire@gmail.com>
2024-03-06 09:09:22 -0300

    add go solution for 0001

---
+++ easy/go/0001-two-sum.go
@@ -0,0 +1,20 @@
+package twoSum
+
+// O(n)
+func twoSum(nums []int, target int) []int {
+   diffMap:= make(map[int]int) 
+
+   for i := 0; i < len(nums); i++ {
+	diff := target - nums[i]
+
+	value, found := diffMap[diff]
+
+	if(found) {
+		return []int{i,value}
+	} else {
+		diffMap[nums[i]] = i
+	}
+   }
+
+   return []int{}
+}

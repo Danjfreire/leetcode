beb0b9217ab902956720309591824ac8ad279c55
Daniel Freire <danjfreire@gmail.com>
2024-03-18 18:59:09 -0300

    add go and js solution for 2390

---
+++ medium/javascript/2390-removing-stars-from-string.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function (s) {
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== "*") {
+      stack.push(s[i]);
+    } else {
+      stack.pop();
+    }
+  }
+
+  return stack.join("");
+};
+
+console.log(removeStars("leet**cod*e"));

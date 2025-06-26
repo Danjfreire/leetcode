4a4a4b04d8392c69a979e7e837f40bb2be14a32e
Danjfreire <danjfreire@gmail.com>
2025-06-26 08:38:15 -0300

    add go solution for 2311

---
+++ medium/go/2311-longest-binary-subsequence-less-than-or-equal-to-k.go
@@ -0,0 +1,23 @@
+package main
+
+func longestSubsequence(s string, k int) int {
+	n := len(s)
+	count := 0
+	power := 0
+	val := int64(0)
+
+	for i := n - 1; i >= 0; i-- {
+		if s[i] == '0' {
+			count++
+		} else {
+			if power < 32 {
+				val += 1 << power
+				if val <= int64(k) {
+					count++
+				}
+			}
+		}
+		power++
+	}
+	return count
+}
